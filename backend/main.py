dd50127b2c1abfcb94fc01298d71f0e662fa78dd
ggabbylopez <gml0426@email.unc.edu>
2023-06-15 21:11:03 -0400

    Minimum functionality

--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,32 @@
-from fastapi import FastAPI, Depends, HTTPException
-from user_service import UserService, User
+import json
+from fastapi import FastAPI, Request
+from TeamsSender import SendMessage
+from bitbucket_webhooks import event_schemas, hooks, router
 
 app = FastAPI()
 
-@app.get("/api/users")
-def get_users(user_service: UserService = Depends()) -> list[User]:
-    return user_service.all()
+@app.post("/webhook/bitbucket")
+def bitbucket(st: str):
+    event = json.loads(st)
+    update_type = ""
+    author = ""
+    repository = ""
+    if 'headers' in event.keys():
+        print(event["headers"])
+        if "X-Event-Key" in event["headers"].keys():
+            print(event['headers']['X-Event-Key'])
+            update_type = event["headers"]["X-Event-Key"]
 
+    if "body" in event.keys():
+        print(event["body"])
+        author = event["body"]["actor"]
+        repository = event["body"]["repository"]
 
-@app.post("/api/users")
-def new_user(user: User, user_service: UserService = Depends()) -> User:
-    try:
-        return user_service.create(user)
-    except Exception as e:
-        raise HTTPException(status_code=422, detail = str(e))
-
-
-@app.get("/api/users/{pid}", responses={404: {"model": None}})
-def get_user(pid: int, user_service: UserService = Depends()) -> User:
-    try:
-        return user_service.get(pid)
-    except Exception as e:
-       raise HTTPException(status_code = 404, detail = str(e))
-
-
-@app.delete("/api/users/{pid}")
-def delete_user(pid: int, user_service: UserService = Depends(UserService)):
-    try:
-        user_service.delete(pid)
-    except Exception as e:
-        raise HTTPException(status_code = 404, detail = str(e))
+    print("Received notification: {} - {} - {}".format(update_type, author, repository))
+    message = "{} Update to {} by {}".format(update_type, repository, author)
+    webhook = "https://eaglefgcu.webhook.office.com/webhookb2/5b668ebc-a9f1-4575-990c-888124a9f2df@f7a5a4ef-4ffa-4c80-bfb3-c12e28872099/IncomingWebhook/69a44ccb361a429bb75418702106fef1/2d04b75f-b6a7-4f37-8386-a11f40753184"
+    SendMessage(message, webhook)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
