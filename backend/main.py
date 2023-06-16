f1b0d53d86340c37a9eb4fd8a05d843a54a2f2f3
Colby Wang <colby.wang765@gmail.com>
2023-06-16 10:00:54 -0400

    Added more PR stuff

--- backend/main.py
+++ backend/main.py
@@ -5,14 +5,87 @@ from TeamsSender import SendMessage
 
 app = FastAPI()
 
-@app.post("/webhook/bitbucket")
-async def bitbucket(request: Request):
+
+@app.post("/webhook/pullRequest")
+async def handle_pr(request: Request):
     json_obj = await request.json()
-    message = "{} has made a pull request ({}) in repo {}".format(json_obj["actor"]["display_name"], json_obj["pullrequest"]["title"], json_obj["repository"]["links"]["html"]["href"])
+    message = "{} opened a pull request to merge ({} -> {})" \
+              "\nLink to PR: {}".format(
+        json_obj["actor"]["display_name"],
+        json_obj["pullrequest"]["source"]["branch"]["name"],
+        json_obj["pullrequest"]["destination"]["branch"]["name"],
+        json_obj["pullrequest"]["links"]["html"]["href"])
 
     webhook = config('WEBHOOK')
     SendMessage(message, webhook)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Ok!')
+    }
+
+
+@app.post("/webhook/pullRequestUpdated")
+async def handle_pr_update(request: Request):
+    json_obj = await request.json()
+    message = "{} has updated a pull request ({})" \
+              "\nLink to PR: {}".format(
+        json_obj["actor"]["display_name"],
+        json_obj["pullrequest"]["title"],
+        json_obj["pullrequest"]["links"]["html"]["href"])
+
+    webhook = config('WEBHOOK')
+    SendMessage(message, webhook)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Ok!')
+    }
+
+
+@app.post("/webhook/pullRequestApproved")
+async def handle_pr_approval(request: Request):
+    json_obj = await request.json()
+    message = "{} has approved a pull request ({})" \
+              "\nLink to PR: {}".format(
+        json_obj["actor"]["display_name"],
+        json_obj["pullrequest"]["title"],
+        json_obj["pullrequest"]["links"]["html"]["href"])
+
+    webhook = config("WEBHOOK")
+    SendMessage(message, webhook)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Ok!')
+    }
+
+
+@app.post("/webhook/pullRequestMerged")
+async def handle_pr_merge(request: Request):
+    json_obj = await request.json()
+    message = "{} has merged a pull request ({})" \
+              "\nLink to PR: {}".format(
+        json_obj["actor"]["display_name"],
+        json_obj["pullrequest"]["title"],
+        json_obj["pullrequest"]["links"]["html"]["href"])
+
+    webhook = config('WEBHOOK')
+    SendMessage(message, webhook)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Ok!')
+    }
+
+
+@app.post("/webhook/pullRequestCommented")
+async def handle_pr_commented(request: Request):
+    json_obj = await request.json()
+    message = "{} has commented on a pull request ({})" \
+              "\nLink to PR: {}".format(
+        json_obj["actor"]["display_name"],
+        json_obj["pullrequest"]["title"],
+        json_obj["pullrequest"]["links"]["html"]["href"])
+    webhook = config('WEBHOOK')
+    SendMessage(message, webhook)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Ok!')
     }
